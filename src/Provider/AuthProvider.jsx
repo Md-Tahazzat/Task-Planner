e76d53a580c6798626a8cadc47d5e68022c8de01
Md-Tahazzat <mdtahazzatali820@gmail.com>
2023-06-15 15:18:39 +0600

    Register page created

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,9 +1,37 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import auth from "../Firebase/Firebase";
 
 export const AuthContext = createContext(null);
+
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    const unsubscribed = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      if (currentUser?.email) {
+        fetch("http://localhost:5000/", {
+          method: "POST",
+          headers: {
+            "Content-Typer": "Application/json",
+          },
+          body: JSON.stringify({ email: currentUser.email }),
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            localStorage.setItem("access-token", data?.token);
+          });
+      }
+      console.log(currentUser);
+    });
+
+    return () => unsubscribed();
+  }, []);
+
   const authInfo = {
-    user: "John Doe",
+    user,
   };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { FaEye, FaEyeSlash, FaGoogle } from "react-icons/fa";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import UseAuthcontext from "../Hooks/UseAuthcontext";
 import UpdateTitle from "../Hooks/UpdateTitle";
+import Swal from "sweetalert2";
+import {
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+import auth from "../Firebase/Firebase";
 
 const Register = () => {
-  const { update, googleSignIn, handleRegister } = UseAuthcontext;
-  const [creatingUser, setCreatingUser] = useState(false);
-  const [errorMsg, setErrorMsg] = useState("");
   const [hidePassword, setHidePassword] = useState(true);
   const location = useLocation();
   const navigate = useNavigate();
@@ -17,188 +20,186 @@ const Register = () => {
     register,
     handleSubmit,
     reset,
+    setError,
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => {
-    setCreatingUser(true);
-    const { confirmPassword, email, image, name, password } = data;
-    setErrorMsg("");
+    const { confirmPassword, email, password } = data;
     if (password !== confirmPassword) {
-      setErrorMsg("Confirm password didn't match");
-      setCreatingUser(false);
+      setError("confirmPassword", {
+        type: "manual",
+        message: "Confirm password didn't match",
+      });
       return;
     }
-    handleRegister(email, password)
+    Swal.fire({
+      title: "processing...",
+      allowOutsideClick: false,
+      showConfirmButton: false,
+      onBeforeOpen: () => {
+        Swal.showLoading();
+      },
+    });
+
+    // create user
+    signInWithEmailAndPassword(auth, email, password)
       .then((user) => {
         if (user) {
-          const userDetails = {
-            displayName: name,
-            photoURL: image,
-          };
-          update(userDetails).then((result) => {
-            setCreatingUser(false);
-            reset();
-            navigate(from, { replace: true });
-          });
+          Swal.close();
+          reset();
+          navigate(from, { replace: true });
         }
       })
       .catch((err) => {
-        setErrorMsg(err.message);
-        setCreatingUser(false);
+        console.log(err);
       });
   };
 
   // social login method
   const handleGoogleSigin = () => {
-    setErrorMsg("");
-    googleSignIn()
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth)
       .then((result) => {
         if (result?.user) {
           navigate(from, { replace: true });
         }
       })
-      .catch((error) => setErrorMsg(err.message));
+      .catch((error) => console.log(err.message));
   };
 
   return (
     <div className="w-full px-4 md:px-auto mt-2 md:mt-0 pb-20">
       <UpdateTitle title="Reister"></UpdateTitle>;
-      {creatingUser ? (
-        <div className="bg-slate-300/80 dark:bg-slate-700/70 rounded-lg py-20 gap-2 w-full mx-auto md:max-w-[30rem] flex flex-col items-center">
-          <span className="text-2xl mr-6">Creating...</span>
-          <div className="w-10 h-10 mr-8 rounded-full animate-spin border-dotted border-blue-500 border-8"></div>
-        </div>
-      ) : (
-        <form
-          className="border md:min-w-[28rem] mx-auto md:max-w-[32rem] p-4 w-full md:py-5 md:px-20 bg-slate-300/90 dark:bg-slate-700/90 border-slate-300 dark:border-slate-600 rounded-md"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <h1 className="text-center text-2xl font-semibold ">
-            Please Register !!
-          </h1>
-          <div className="w-full">
-            <label htmlFor="name" className="label">
-              Name:
-            </label>
-            <input
-              type="text"
-              id="name"
-              required
-              {...register("name", { required: true })}
-              placeholder="Enter your name"
-              className="py-2 px-2 bg-slate-100/60 dark:bg-slate-500/60 rounded-md border dark:border-slate-500 border-slate-300 focus:outline-none focus:border-slate-400 w-full max-w-lg"
-            />
-          </div>
-
-          <div className="w-full">
-            <label htmlFor="email" className="label">
-              Email:
-            </label>
-            <input
-              type="email"
-              id="email"
-              required
-              {...register("email", { required: true })}
-              placeholder="Enter your email"
-              className="py-2 px-2 bg-slate-100/60 dark:bg-slate-500/60 rounded-md border dark:border-slate-500 border-slate-300 focus:outline-none focus:border-slate-400 w-full max-w-lg"
-            />
-          </div>
-          <div className="w-full relative">
-            <label className="label" htmlFor="password">
-              Password:
-            </label>
-            <input
-              type={hidePassword ? "password" : "text"}
-              required
-              {...register("password", {
-                required: true,
-                minLength: {
-                  value: 6,
-                  message: "Password must be at least 6 characters",
-                },
-                pattern: {
-                  value: /^(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
-                  message:
-                    "Password must contain at least one uppercase letter and one special character",
-                },
-              })}
-              placeholder="Password"
-              className={`py-2 px-2 bg-slate-100/60 dark:bg-slate-500/60 w-full rounded-md border ${
-                errorMsg || errors?.password
-                  ? "border-red-600"
-                  : "dark:border-slate-500 border-slate-400"
-              } focus:outline-none focus:border-slate-400 max-w-sm`}
-            />
-            <a
-              onClick={() => setHidePassword(!hidePassword)}
-              href="#"
-              className="absolute right-3 bottom-3"
-            >
-              {hidePassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
-            </a>
-          </div>
-
-          <div className="w-full relative">
-            <label className="label" htmlFor="password">
-              Confirm-Password:
-            </label>
-            <input
-              type={hidePassword ? "password" : "text"}
-              required
-              {...register("confirmPassword", {
-                required: true,
-                minLength: {
-                  value: 6,
-                  message: "Password must be at least 6 characters",
-                },
-                pattern: {
-                  value: /^(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
-                  message:
-                    "Password must contain at least one uppercase letter and one special character",
-                },
-              })}
-              placeholder="Confirm password"
-              className={`py-2 px-2 bg-slate-100/60 dark:bg-slate-500/60 w-full rounded-md border ${
-                errorMsg || errors?.password
-                  ? "border-red-600"
-                  : "dark:border-slate-500 border-slate-400"
-              } focus:outline-none focus:border-slate-400 max-w-sm`}
-            />
-            <a
-              onClick={() => setHidePassword(!hidePassword)}
-              href="#"
-              className="absolute right-3 bottom-3"
-            >
-              {hidePassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
-            </a>
-          </div>
-          <div>
-            <label className="label text-red-500 dark:text-red-400">
-              {errorMsg ||
-                (errors && (
-                  <span>{errorMsg || errors?.password?.message}</span>
-                ))}
-            </label>
-          </div>
-
-          <p className="text-sm">
-            Already have an account?
-            <Link
-              state={{ from: from }}
-              className="text-blue-600 dark:text-blue-400 ml-1"
-              to="/login"
-            >
-              Login
-            </Link>
-          </p>
-
+      <form
+        className="border md:min-w-[28rem] mx-auto md:max-w-[32rem] p-4 w-full md:py-5 md:px-20 bg-slate-300/90 dark:bg-slate-700/90 border-slate-300 dark:border-slate-600 rounded-md"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <h1 className="text-center text-2xl font-semibold ">
+          Please Register !!
+        </h1>
+        <div className="w-full">
+          <label htmlFor="name" className="label">
+            Name:
+          </label>
           <input
-            className="w-full mt-4 py-2 max-w-md  bg-blue-600 rounded-md text-white text-xl hover:bg-blue-700"
-            type="submit"
-            value="Register"
+            type="text"
+            id="name"
+            required
+            {...register("name", { required: true })}
+            placeholder="Enter your name"
+            className="py-2 px-2 bg-slate-100/60 dark:bg-slate-500/60 rounded-md border dark:border-slate-500 border-slate-300 focus:outline-none focus:border-slate-400 w-full max-w-lg"
           />
-        </form>
-      )}
+        </div>
+
+        <div className="w-full">
+          <label htmlFor="email" className="label">
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            required
+            {...register("email", { required: true })}
+            placeholder="Enter your email"
+            className="py-2 px-2 bg-slate-100/60 dark:bg-slate-500/60 rounded-md border dark:border-slate-500 border-slate-300 focus:outline-none focus:border-slate-400 w-full max-w-lg"
+          />
+        </div>
+        <div className="w-full relative">
+          <label className="label" htmlFor="password">
+            Password:
+          </label>
+          <input
+            type={hidePassword ? "password" : "text"}
+            required
+            {...register("password", {
+              required: true,
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters",
+              },
+              pattern: {
+                value: /^(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+                message:
+                  "Password must contain at least one uppercase letter and one special character",
+              },
+            })}
+            placeholder="Password"
+            className={`py-2 px-2 bg-slate-100/60 dark:bg-slate-500/60 w-full rounded-md border ${
+              errors?.password
+                ? "border-red-600"
+                : "dark:border-slate-500 border-slate-400"
+            } focus:outline-none focus:border-slate-400 max-w-sm`}
+          />
+          <a
+            onClick={() => setHidePassword(!hidePassword)}
+            href="#"
+            className="absolute right-3 bottom-3"
+          >
+            {hidePassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+          </a>
+        </div>
+
+        <div className="w-full relative">
+          <label className="label" htmlFor="password">
+            Confirm-Password:
+          </label>
+          <input
+            type={hidePassword ? "password" : "text"}
+            required
+            {...register("confirmPassword", {
+              required: true,
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters",
+              },
+              pattern: {
+                value: /^(?=.*[A-Z])(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/,
+                message:
+                  "Password must contain at least one uppercase letter and one special character",
+              },
+            })}
+            placeholder="Confirm password"
+            className={`py-2 px-2 bg-slate-100/60 dark:bg-slate-500/60 w-full rounded-md border ${
+              errors?.password || errors.confirmPassword
+                ? "border-red-600"
+                : "dark:border-slate-500 border-slate-400"
+            } focus:outline-none focus:border-slate-400 max-w-sm`}
+          />
+          <a
+            onClick={() => setHidePassword(!hidePassword)}
+            href="#"
+            className="absolute right-3 bottom-3"
+          >
+            {hidePassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+          </a>
+        </div>
+        <div>
+          <label className="label text-red-500 dark:text-red-400">
+            {errors && (
+              <span>
+                {errors.confirmPassword?.message || errors?.password?.message}
+              </span>
+            )}
+          </label>
+        </div>
+
+        <p className="text-sm">
+          Already have an account?
+          <Link
+            state={{ from: from }}
+            className="text-blue-600 dark:text-blue-400 ml-1"
+            to="/login"
+          >
+            Login
+          </Link>
+        </p>
+
+        <input
+          className="w-full mt-4 py-2 max-w-md  bg-blue-600 rounded-md text-white text-xl hover:bg-blue-700"
+          type="submit"
+          value="Register"
+        />
+      </form>
       <p className="text-center">Sign in with </p>
       <div className="my-4 max-w-[32rem] w-full mx-auto flex items-center justify-center gap-5">
         <button
