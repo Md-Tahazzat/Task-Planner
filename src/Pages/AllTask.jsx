8ba68aef7d07457af86cbcd9d08250bbc67f912a
Md-Tahazzat <mdtahazzatali820@gmail.com>
2023-07-11 16:13:21 +0600

    Navbar added with ddark and light theme functionality

---
+++ src/Pages/AllTask.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AllTask = () => {
+  return (
+    <div>
+      <h1>From All task</h1>
+    </div>
+  );
+};
+
+export default AllTask;
--- src/Router/Route.jsx
+++ src/Router/Route.jsx
@@ -1,9 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
+import Root from "../Root/Root";
+import AllTask from "../Pages/AllTask";
+import AddTask from "../Pages/AddTask";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <p>Hello world</p>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <AllTask></AllTask>,
+      },
+      {
+        path: "add-task",
+        element: <AddTask></AddTask>,
+      },
+    ],
   },
 ]);
 
